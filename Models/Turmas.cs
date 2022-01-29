ceaceb48814e38b48761decd9112f84258c6cad7
Jean Fraga <jeanfragoli395@gmail.com>
2022-01-29 17:17:21 -0300

    first commit

---
+++ Models/Turmas.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace jeanFraga.Models
+{
+    public class Turmas
+    {
+          [Key]
+          public int Id {get; set; }
+
+          [MinLength(2, ErrorMessage = "The class name must be at minimum 2 characteres")]
+          [MaxLength(20, ErrorMessage = "The class name must be at maximum of 20 characteres")]
+          [Required(ErrorMessage = "Class name is required")]
+          public string Name { get; set; }
+
+
+    }
+}
